b7812144c297713b3dbb183aec89c5581fd0f9f4
PRIYA <priyapillarisetty19@gmail.com>
2025-10-29 15:25:10 -0400

    Added sayHello Controller and Jsp

---
+++ ToDoX/src/main/java/com/priya/todox/hello/SayHelloController.java
@@ -0,0 +1,44 @@
+package com.priya.todox.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SayHelloController {
+	
+	// "say-hello" => "Hello! What are you learning today?"
+	
+	@RequestMapping("say-hello")
+	@ResponseBody
+	//We are adding the responsebody tag to prevent spring to search for a 
+	//view named :Hello? What are you learning today? instead of returning the string
+	
+	public String sayHello() {
+		return "Hello! What are you learning today?";
+	}
+	
+	@RequestMapping("say-hello-html")
+	@ResponseBody
+	public String sayHelloHtml() {
+		StringBuffer sb = new StringBuffer();
+		sb.append("<html>");
+		sb.append("<head>");
+		sb.append("<title> My first HTML Page</title>");
+		sb.append("</head>");
+		sb.append("<body>");
+		sb.append("My first html page with body");
+		sb.append("</body>");
+		sb.append("</html>");
+
+		return sb.toString();
+	}
+	
+	//sayHello.jsp
+	//"say-hello-jsp" => sayHello.jsp
+	@RequestMapping("say-hello-jsp")
+	public String sayHelloJsp() {
+		return "sayHello";
+	}
+	
+}
